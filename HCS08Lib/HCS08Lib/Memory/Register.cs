9ba266d45f07133264f10b08a8a33c7b917c9f9c
adam <herp@derp.co>
2016-04-17 14:40:16 -0600

    moving downstairs

--- HCS08Lib/HCS08Lib/Memory/Register.cs
+++ HCS08Lib/HCS08Lib/Memory/Register.cs
@@ -8,6 +8,39 @@ namespace HCS08Lib.Memory
 {
     public class Register : MemorySpace
     {
+        Dictionary<UInt16, Peripheral.iCPU> peripherals = new Dictionary<UInt16, Peripheral.iCPU>();
+
         public Register(uint memory_size) : base(memory_size) { }
+
+        public void AddPeripheral(Peripheral.Peripheral p)
+        {
+            //Note that peripherals isn't thread protected.  Add all of these before execution starts
+            foreach (Peripheral.iCPU icpu in p.GetCPURegisters())
+            {
+                if (peripherals.ContainsKey(icpu.RegisterOffset))
+                    throw new InvalidOperationException("Register space already has a peripheral at offset " + icpu.RegisterOffset);
+                else if (icpu.RegisterOffset >= Length)
+                    throw new IndexOutOfRangeException();
+                else
+                    peripherals[icpu.RegisterOffset] = icpu;
+            }
+        }
+
+        public override byte this[ushort i]
+        {
+            get
+            {
+                if (peripherals.ContainsKey(i))
+                    return peripherals[i].Value;
+                return base[i];
+            }
+
+            set
+            {
+                if (peripherals.ContainsKey(i))
+                   peripherals[i].Value = value;
+                base[i] = value;
+            }
+        }
     }
 }
---
+++ HCS08Lib/HCS08Lib/Peripheral/Peripheral.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCS08Lib.Peripheral
+{
+    public abstract class iCPU
+    {
+        public abstract byte Value
+        {
+            get;
+            set;
+        }
+
+        public abstract UInt16 RegisterOffset
+        {
+            //Effectively becomes the address when used for low-page registers
+            //Effectively gets an offset of 0x1800 when used for high-page registers
+            get;
+        }
+    }
+
+    public abstract class Peripheral
+    {
+        List<iCPU> CPURegisters = new List<iCPU>();
+
+        public List<iCPU> GetCPURegisters()
+        {
+            return CPURegisters;
+        }
+    }
+}
